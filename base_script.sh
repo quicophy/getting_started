931950f10d28915835d663d478f2a75d253ad7bc
benjaminlanthier <lanthier_benji@hotmail.com>
2023-09-26 17:53:01 -0400

    Structured the script

--- base_script.sh
+++ base_script.sh
@@ -4,9 +4,11 @@
 #SBATCH --time=01:00:00                # Time given to run your job (1 hour in this case)
 #SBATCH --account=def-ko1              # Keep this as it is
 #SBATCH --cpus-per-task=1              # Asked CPUs per task for your job
-#SBATCH --mem=40GB # Memory per node   # Memory asked for your job
+#SBATCH --mem=8GB # Memory per node   # Memory asked for your job
 #SBATCH --mail-user=<your email>       # Receive a notification when your jobs finish running
 #SBATCH --mail-type=ALL
+
+
 
 # Define virtual environment name and the files' names
 env_name="venv"            # Virtual environment name
